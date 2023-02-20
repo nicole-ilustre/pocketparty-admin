3ec8eba6ffad9b139d074bb3a74f7c0f04883b93
Sudesh Poojari <poojari.sudesh@gmail.com>
2023-02-20 18:24:03 +0530

    chore: added bookmark record

--- src/pages/api/add-game.js
+++ src/pages/api/add-game.js
@@ -4,5 +4,12 @@ export default async function handler(req, res) {
   const gamesRef = db.collection("games");
   const response = await gamesRef.add(JSON.parse(req.body));
 
+  const gameBookmarkRef = db.collection("game-bookmarks");
+
+  await gameBookmarkRef.doc(response.id).set({
+    count: 0,
+    weight: 0,
+  });
+
   res.status(200).json({ status: "DONE", uid: response.id });
 }
