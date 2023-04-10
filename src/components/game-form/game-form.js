8df517a52e50d89a8eb675f192371851c8a4859e
Sudesh Poojari <poojari.sudesh@gmail.com>
2023-04-10 19:11:13 +0530

    chore: self review

--- src/components/game-form/game-form.js
+++ src/components/game-form/game-form.js
@@ -36,16 +36,9 @@ const DEFAULT_GAME = {
 };
 
 const style = {
-  // position: "absolute",
-  // top: "50%",
-  // left: "50%",
-  // transform: "translate(-50%, -50%)",
   width: "560px",
-  // bgcolor: "background.paper",
-  // boxShadow: 24,
   maxHeight: "90%",
   padding: "12px",
-  // p: 3,
 };
 
 const getSlug = (title) =>
